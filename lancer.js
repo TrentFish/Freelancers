aed6ec6c2f3977d0431e96149afcac896f5ebc2b
fishyboi@MSI <fishyboi@MSI>
2023-08-24 15:29:57 -0400

    Figuring out the basics

---
+++ lancer.js
@@ -0,0 +1,43 @@
+const container = document.querySelector('.container');
+const nameList = document.querySelector('.name');
+const priceList = document.querySelector('.price');
+const jobList = document.querySelector('.job');
+
+const names = [
+    "PeePee",
+    "Dr. Eggman",
+    "Prof",
+    "Roland",
+    "Shelly",
+    "Mr. Bowers",
+    "PooPoo",
+    "Joenathan",
+    "Karen",
+    "Hillary",
+    "Simon",
+    "Conway",
+    "Mr. Beef",
+    "Dr. Skel E. Ton",
+    "Mrs. Skill",
+    "Donny",
+    "Leland",
+    "Sir Gun",
+    "Mario",
+    "Daisy"
+  ];
+  
+  const occupations = [
+    "gardener",
+    "programmer",
+    "teacher",
+    "driver",
+    "chef",
+    "editor",
+    "voice actor",
+    "pet sitter",
+    "electrician",
+    "janitor"
+  ];
+
+  console.log(names);
+  console.log(occupations);
